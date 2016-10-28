ef1783941ee1c0089d3772e24653cbdc06c84a48
CheezBoiger <alexander.garcia18@yahoo.com>
2016-10-28 01:17:44 -0700

    Meant to write Matrix4 not Mat4

--- vikr-source/math/vikr_math.hpp
+++ vikr-source/math/vikr_math.hpp
@@ -14,7 +14,7 @@ namespace vikr {
 using Vect4 = vikr::math::Vector4<float>;
 using Vect3 = vikr::math::Vector3<float>;
 using Vect2 = vikr::math::Vector2<float>;
-using Mat4 = vikr::math::Mat4<float>;
+using Mat4 = vikr::math::Matrix4<float>;
 using Mat3 = vikr::math::Matrix3<float>;
 using Mat2 = vikr::math::Matrix2<float>;
 } // vikr
