c51ad45151678607248c64a89a0cc5ce87fa177d
CheezBoiger <alexander.garcia18@yahoo.com>
2016-10-29 23:20:16 -0700

    Setting up to use glm, math folder is not in use

--- vikr-source/math/alg/vect.hpp
+++ vikr-source/math/alg/vect.hpp
@@ -12,31 +12,52 @@ namespace math {
 template<typename _Value = float>
 class Vector2 {
 public:
-private:
-  _Value x;
-  _Value y;
+  Vector2(_Value x = 1.0f,
+          _Value y = 1.0f)
+  : x(x), y(y) { }
+
+
+  union {
+    struct { _Value x, y; };
+    struct { _Value r, g; };
+    struct { _Value s, t; };
+  };
 };
 
 
 template<typename _Value = float>
 class Vector3 {
 public:
-private:
-  _Value x;
-  _Value y;
-  _Value z;
+  Vector3(_Value x = 1.0f, 
+          _Value y = 1.0f, 
+          _Value z = 1.0f)
+  : x(x), y(y), z(z) { }
+
+
+  union {
+    struct { _Value x, y, z; };
+    struct { _Value r, g, b; };
+    struct { _Value s, t, r; };
+  };
 };
 
 
 template<typename _Value = float>
 class Vector4 {
 public:
-private:
+  // Vector4 contruction.
+  Vector4(_Value x = 1.0f, 
+          _Value y = 1.0f, 
+          _Value z = 1.0f , 
+          _Value w = 1.0f)
+  : x(x), y(y), z(z), w(w) { }
 
-  _Value x;
-  _Value y;
-  _Value z;
-  _Value w;
+
+  union {
+    struct { _Value x, y, z, w; };
+    struct { _Value r, g, b, a; };
+    struct { _Value s, t, r, q; };
+  };
 };
 } // math
 } // vikr
